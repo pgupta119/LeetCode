a0a2343dbd2c3e22eb11ce2d7ad969c933c2f6ea
pgupta119 <guptaprakash1995119@gmail.com>
2021-03-10 01:11:32 +0100

    Decoder for machine translation

--- DailyPratice/seqtoseqmachinetranslation.py
+++ DailyPratice/seqtoseqmachinetranslation.py
@@ -30,7 +30,7 @@ english=Field(tokenize=tokenier_eng,lower=True,
 
 
 train_data, validate_data,test_data=Multi30k.splits(exts=(".de",".en"),
-                                                    fields=(german, english))
+                                                    fields=(german, english))     
 
 
 german.built_vocab(train_data,max_size=10000,min_freq=2)
@@ -64,6 +64,63 @@ class EncoderLSTM(nn.module):
         
         #Shape----- [300,2,1024]
         self.LSTM=nn.LSTM(self.embedding_size,hidden_size,num_layer)
+
+    def forward(self,x):
+    #shape of [sequence length,batch_size][26,32]
+
+    #shape of [sequence length,batch_size][26,32,300] embedding_size
+        embedding=self.dropout(self.embedding(x))
+
+    #Shape --> outputs (26, 32, 1024) [Sequence_length , batch_size , hidden_size]
+    #Shape --> (hs, cs) (2, 32, 1024) , (2, 32, 1024) [num_layers, batch_size size, hidden_size]
+        outputs, (hidden_state, cell_state) = self.LSTM(embedding)
+
+    return hidden_state, cell_state
+
+class DecoderLSTM(nn.Module):
+    def __init__ (self, input_size,embedding_size,hidden_size, num_layers, p, output_size):
+        super(DecoderLSTM,self).__init__()
+
+
+        self.input_size=input_size
+        self.output_size=output_size
+        self.embedding_size=embedding_size
+        self.hidden_layer=hidden_layer
+        self.num_layers=num_layers
+        self.embedding=nn.Embedding(self.input_size,self.embedding_size)
+        self.dropout=Dropout(p)
+        self.tag=True
+        self.LSTM=nn.LSTM(Self.embedding_size, num_layers,droput=p)
+        self.fc=nn.Linear(self.hidden_size,self.output_size)
+
+
+    def forward(self, hidden_state, cell_state):
+
+        x=x.unsqueezw(0)
+        embedding=self.droput(self.embedding(x))
+        outputs, (hidden_state,cell_state)=self.LSTM(embedding,(hidden_state,cell_state))
+        predictions=self.fc(outputs)
+        predictions=predictions.squeeze(0)
+
+    return  predictions, hidden_state, cell_state
+
+
+
+
+class seq2seq(nn.Module):
+    def __init__(self, source, target, tfr=0.5):
+        batch_size=source.shape[1]
+        target_len=target.shape[0]
+        target_vocab_size=len(english.vocab)
+        outputs=torch.zeros (target_len,batch_size,target_vocab_size).to(device)
+        hidden_state_encoder,cell_state_encoder=self.DecoderLSTM        
+
+    
+
+
+
+
+
 
 
         

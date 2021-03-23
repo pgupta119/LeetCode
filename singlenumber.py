129b3efaf7fae8322886d166efaab170de330268
pgupta119 <guptaprakash1995119@gmail.com>
2021-03-23 22:01:18 +0100

    Leet code solution double number

--- singlenumber.py
+++ singlenumber.py
@@ -21,3 +21,21 @@ class Solution:
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return 2*sum(set(nums))-sum(nums)  
+
+
+
+# Given an integer array nums where every element appears three times
+#  except for one, which appears exactly once. Find the single element 
+#  and return it.
+
+
+# Example 1:
+
+# Input: nums = [2,2,3,2]
+# Output: 3
+
+
+Solution:
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return int((3*sum(set(nums))-sum(nums))/2)

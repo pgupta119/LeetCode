ed684caa6dc00083d66189e687be5fcee968109b
Prakash Gupta <62263152+pgupta119@users.noreply.github.com>
2021-04-07 20:58:58 +0200

    Update Buddystring.py
    Added  comments on the solution.

--- Buddystring.py
+++ Buddystring.py
@@ -4,23 +4,26 @@
 
 
 #Example 
-
 # Input: a = "ab", b = "ba"
-# Output: true
+# Output: True
 # Explanation: You can swap a[0] = 'a' and a[1] = 'b' to get "ba", which is equal to b.
 
-
-
-
-
 class Solution:
-    def buddyStrings(self, a: str, b: str) -> bool:
+    def buddyStrings(a, b):
+        #Length of a and b is not equal
         if len(a)!= len(b): return False
+        #check a and b are equal and distinct element in a is not equal to length of a
         if a==b and len(set(a))!=len(a):
             return True
         indices,indices1=[],[]
+        #check for each element where a and b are not equal, put into the new list
+        #new list should contain 2 values and compare the first new list with reverse  second new list
         for i in range(len(a)):
             if a[i] != b[i]:
                 indices.append(a[i])
                 indices1.append(b[i])
+                #return the true and false
         return len(indices)==2 and indices==indices1[::-1]
+    
+ print(Solution.buddystrings("ab","ba))
+ #Output :True

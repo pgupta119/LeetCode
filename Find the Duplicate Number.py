730d706faa8256238ac836b672c72768f633ef83
Prakash Gupta <62263152+pgupta119@users.noreply.github.com>
2021-04-07 21:02:26 +0200

    Update Find the Duplicate Number.py

--- Find the Duplicate Number.py
+++ Find the Duplicate Number.py
@@ -8,7 +8,8 @@
 
 #Solution 1
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+    def findDuplicate(self,nums):
+        #checking for element and if it present more than one times in the list the count becomes 2 and that will be the output
         for i in nums:
             count=0
             for j in nums :
@@ -19,6 +20,6 @@ class Solution:
 
 
 #comment : This will work for small value of the list
-
+print(Solution.findDuplicate([1,3,4,2,2]))
 
 
